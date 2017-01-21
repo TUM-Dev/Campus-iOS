a28d67f1e19d237efbfd8489b73d3e72c9cb8d3a
mammuth <kontakt@maxi-muth.de>
2017-01-21 12:29:36 +0100

    Run push builds on travis only when merging into master

--- fastlane/travis_runner.sh
+++ fastlane/travis_runner.sh
@@ -3,9 +3,9 @@
 set -e # Exit if a command returns a non-zero code
 
 if [[ ${TRAVIS_BRANCH} == 'master' ]]; then
-    FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 travis_retry fastlane integration
+    FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 fastlane integration
 elif [[ ${TRAVIS_PULL_REQUEST} != 'false' ]]; then
-    FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 travis_retry fastlane pull_request
+    FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 fastlane pull_request
 else 
 	echo 'Not running any tasks, since this request seems to be not for the master branch neither a pull request'
 fi
