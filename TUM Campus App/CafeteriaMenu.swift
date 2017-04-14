fa74ebd24fa85e14f2dd95091e4aa9ca12f9a792
Tim <TG908@users.noreply.github.com>
2017-04-14 23:27:46 +0200

    Added Price property

--- TUM Campus App/CafeteriaMenu.swift
+++ TUM Campus App/CafeteriaMenu.swift
@@ -15,14 +15,17 @@ class CafeteriaMenu: DataElement {
     let typeLong: String
     let typeNr: Int
     let typeShort: String
+    let price: Price?
     
-    init(id: Int, date: Date, typeShort: String, typeLong: String, typeNr: Int, name: String) {
+    
+    init(id: Int, date: Date, typeShort: String, typeLong: String, typeNr: Int, name: String, price: Price?) {
         self.id = id
         self.date = date
         self.typeShort = typeShort
         self.typeLong = typeLong
         self.typeNr = typeNr
         self.name = name
+        self.price = price
     }
     
     func getCellIdentifier() -> String {
