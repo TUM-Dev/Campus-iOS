ea277589f81a01fa0421ace432686805a595cf55
Tim <TG908@users.noreply.github.com>
2017-04-23 01:32:21 +0200

    Added LibrarySettingsVC

---
+++ TUM Campus App/LibrarySettingsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LibrarySettingsViewController.swift
+//  TUM Campus App
+//
+//  Created by Tim Gymnich on 23.04.17.
+//  Copyright © 2017 LS1 TUM. All rights reserved.
+//
+
+import UIKit
+
+class LibrarySettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
