8740283dd2bca8f7cccbe010b32d59913c9259f2
Tim <TG908@users.noreply.github.com>
2017-04-23 01:35:46 +0200

    Added LibrarySettingsViewController

--- TUM Campus App/LibrarySettingsViewController.swift
+++ TUM Campus App/LibrarySettingsViewController.swift
@@ -9,27 +9,20 @@
 import UIKit
 
 class LibrarySettingsViewController: UIViewController {
+    
+    @IBOutlet var usernameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
 
 }
