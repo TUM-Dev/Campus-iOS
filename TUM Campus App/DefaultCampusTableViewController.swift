6a0144ed51e0d17ece0a7dbfadfe092bbe406e30
Tim <TG908@users.noreply.github.com>
2017-05-04 10:08:12 +0200

    expandable pickers

--- TUM Campus App/DefaultCampusTableViewController.swift
+++ TUM Campus App/DefaultCampusTableViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class DefaultCampusTableViewController: UITableViewController {
+    
+    @IBOutlet var campusSelectorCell: UITableViewCell!
+    var campusSelectorExpanded = false
+    @IBOutlet var cafeteriaSelectorCell: UITableViewCell!
+    var cafeteriaSelectorExpanded = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,80 +21,50 @@ class DefaultCampusTableViewController: UITableViewController {
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        switch indexPath.row {
+            
+        case 0:
+            campusSelectorExpanded = !campusSelectorExpanded
+            UIView.animate(withDuration: 0.5) {
+                tableView.reloadRows(at: [indexPath], with: .fade)
+                tableView.reloadData()
+            }
+        case 2:
+            cafeteriaSelectorExpanded = !cafeteriaSelectorExpanded
+            UIView.animate(withDuration: 0.5) {
+                tableView.reloadRows(at: [indexPath], with: .fade)
+                tableView.reloadData()
+            }
+        default:
+            break
+            
+        }
+        
     }
-
-    // MARK: - Table view data source
-
-//    override func numberOfSections(in tableView: UITableView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return 1
-//    }
-//
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        // #warning Incomplete implementation, return the number of rows
-//        return 4
-//    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        switch indexPath.row {
+        case 1:
+            if campusSelectorExpanded {
+                return 220
+            } else {
+                return 0
+            }
+        case 3:
+            if cafeteriaSelectorExpanded {
+                return 220
+            } else {
+                return 0
+            }
+        default:
+            return 44
+        }
+        
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
