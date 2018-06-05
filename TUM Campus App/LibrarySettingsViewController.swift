fdb0a11e7a8f507eceb77f1bf6049b0bfa8a7189
Tim Gymnich <TG908@users.noreply.github.com>
2018-06-05 19:29:12 +0200

    Updated license. Fixes #69

--- TUM Campus App/LibrarySettingsViewController.swift
+++ TUM Campus App/LibrarySettingsViewController.swift
@@ -1,10 +1,21 @@
-
 //
 //  LibrarySettingsViewController.swift
 //  TUM Campus App
 //
-//  Created by Tim Gymnich on 23.04.17.
-//  Copyright © 2017 LS1 TUM. All rights reserved.
+//  This file is part of the TUM Campus App distribution https://github.com/TCA-Team/iOS
+//  Copyright (c) 2018 TCA
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, version 3.
+//
+//  This program is distributed in the hope that it will be useful, but
+//  WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//  General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program. If not, see <http://www.gnu.org/licenses/>.
 //
 
 import UIKit
