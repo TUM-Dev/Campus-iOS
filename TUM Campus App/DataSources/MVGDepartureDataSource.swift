8b996582beb32638ac1d4e50a7d459cb5cb6776d
Florian Fittschen <ffittschen@gmail.com>
2018-10-17 10:37:36 +0200

    Extract and reuse DepartureView in Card and Detail

--- TUM Campus App/DataSources/MVGDepartureDataSource.swift
+++ TUM Campus App/DataSources/MVGDepartureDataSource.swift
@@ -41,22 +41,11 @@ class MVGDepartureDataSource: NSObject, UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView,
                         cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(
-            withReuseIdentifier: cellReuseID, for: indexPath) as! MVGDepartureCollectionViewCell
-        let departure = data[indexPath.row]
-        
-        cell.destinationLabel.text = departure.destination
-        cell.lineLabel.backgroundColor = UIColor(hexString: departure.lineBackgroundColor)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellReuseID,
+                                                      for: indexPath) as! MVGDepartureCollectionViewCell
 
-        if ["u", "s"].contains(departure.product) {
-            cell.lineLabel.text = "\(departure.product.uppercased())\(departure.label)"
-        } else {
-            cell.lineLabel.text = departure.label
-        }
-        
-        cell.departureLabel.text = DateComponentsFormatter.shortTimeFormatter.string(from: departure.departureTime.timeIntervalSinceNow) ?? ""
+        cell.configure(with: data[indexPath.row])
+
         return cell
     }
-    
 }
-
--- TUM Campus App/MVGDepartureTableViewCell.swift
+++ TUM Campus App/MVGDepartureTableViewCell.swift
@@ -12,50 +12,9 @@ class MVGDepartureTableViewCell: UITableViewCell {
 
     static let Identifier = "MVGDepartureTableViewCell"
 
-    @IBOutlet private weak var lineLabel: UILabel!
-    @IBOutlet private weak var destinationLabel: UILabel!
-    @IBOutlet private weak var departureLabel: UILabel!
-
-    private var departure: Departure?
-    private var timer: Timer?
-    private var timerUntilCountdown: Timer?
-
-    deinit {
-        timer?.invalidate()
-    }
-
-    override func prepareForReuse() {
-        super.prepareForReuse()
-        timer?.invalidate()
-        departure = nil
-        lineLabel.text = ""
-        destinationLabel.text = ""
-        departureLabel.text = ""
-    }
+    @IBOutlet private weak var departureView: DepartureView!
 
     func configure(with departure: Departure) {
-        self.departure = departure
-
-        destinationLabel.text = departure.destination
-        lineLabel.backgroundColor = UIColor(hexString: departure.lineBackgroundColor)
-
-        if ["u", "s"].contains(departure.product) {
-            lineLabel.text = "\(departure.product.uppercased())\(departure.label)"
-        } else {
-            lineLabel.text = departure.label
-        }
-
-        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [weak self] _ in
-            self?.updateTimeRemaining()
-        }
-
-        updateTimeRemaining()
-    }
-
-    private func updateTimeRemaining() {
-        guard let departure = departure else { return }
-        let secondsLeft = departure.departureTime.timeIntervalSinceNow
-
-        departureLabel.text = DateComponentsFormatter.shortTimeFormatter.string(from: secondsLeft) ?? ""
+        departureView.configure(with: departure)
     }
 }
