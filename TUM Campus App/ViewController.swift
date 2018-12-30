933b55c13c2f68ae7dafc27d7ec1932505632b78
Tim Gymnich <TG908@users.noreply.github.com>
2018-12-30 18:48:33 +0100

    setup new project

---
+++ TUM Campus App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TUM Campus App
+//
+//  Created by Tim Gymnich on 12/30/18.
+//  Copyright © 2018 TUM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
