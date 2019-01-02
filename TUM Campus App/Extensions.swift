b3b91a160fed01544f17937bc8810c731c964ac7
Tim Gymnich <TG908@users.noreply.github.com>
2019-01-02 22:48:53 +0100

    Added xml serializer extension

--- TUM Campus App/Extensions.swift
+++ TUM Campus App/Extensions.swift
@@ -7,6 +7,9 @@
 //
 
 import Foundation
+import Ono
+import SWXMLHash
+import Alamofire
 
 extension Bundle {
     
@@ -22,4 +25,48 @@ extension Bundle {
         return "TCA iOS \(version)/\(build)"
     }
     
+}
+
+enum BackendError: Error {
+    case network(error: Error)
+    case dataSerialization(error: Error)
+    case jsonSerialization(error: Error)
+    case xmlSerialization(error: Error)
+    case objectSerialization(reason: String)
+}
+
+extension DataRequest {
+    static func xmlResponseSerializer() -> DataResponseSerializer<XMLIndexer> {
+        return DataResponseSerializer { (_, response, data, error) -> Result<XMLIndexer> in
+            
+            let result = Request.serializeResponseData(response: response, data: data, error: nil)
+            
+            guard case let .success(validData) = result else {
+                return .failure(BackendError.dataSerialization(error: result.error! as! AFError))
+            }
+            
+            let xmlParser = SWXMLHash.config { config in config.detectParsingErrors = true }
+            let xml = xmlParser.parse(validData)
+            
+            switch xml {
+            case let .xmlError(error):
+                return .failure(BackendError.xmlSerialization(error: error))
+            default:
+                return .success(xml)
+            }
+        }
+    }
+    
+    @discardableResult
+    func responseXML(
+        queue: DispatchQueue? = nil,
+        completionHandler: @escaping (DataResponse<XMLIndexer>) -> Void)
+        -> Self
+    {
+        return response(
+            queue: queue,
+            responseSerializer: DataRequest.xmlResponseSerializer(),
+            completionHandler: completionHandler
+        )
+    }
 }
