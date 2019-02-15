dea7fdf1468d55352e7a218e930407d16d011c67
Tim Gymnich <TG908@users.noreply.github.com>
2019-02-15 23:29:34 +0100

    Added CalendarImporter and Event

---
+++ TUM Campus App/Calendar/CalendarImporter.swift
@@ -0,0 +1,9 @@
+//
+//  CalendarImporter.swift
+//  TUM Campus App
+//
+//  Created by Tim Gymnich on 2/15/19.
+//  Copyright © 2019 TUM. All rights reserved.
+//
+
+import Foundation
---
+++ TUM Campus App/Calendar/Event.swift
@@ -0,0 +1,38 @@
+//
+//  Event.swift
+//  TUM Campus App
+//
+//  Created by Tim Gymnich on 2/15/19.
+//  Copyright © 2019 TUM. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc class Event: NSManagedObject/*, Decodable*/ {
+    
+/*
+     <event>
+     <nr>886515989</nr>
+     <status>FT</status>
+     <url>https://campus.tum.de/tumonline/lv.detail?cLvNr=950369994</url>
+     <title>Programmoptimierung (IN2053) VI</title>
+     <description>fix; Abhaltung; </description>
+     <dtstart>2019-01-16 10:00:00</dtstart>
+     <dtend>2019-01-16 12:00:00</dtend>
+     <location>00.13.009A, Seminarraum (5613.EG.009A)</location>
+     </event>
+*/
+
+/*
+    @NSManaged public var descriptionText: String?
+    @NSManaged public var dstart: Date?
+    @NSManaged public var dtend: Date?
+    @NSManaged public var location: String?
+    @NSManaged public var nr: Int64
+    @NSManaged public var status: String?
+    @NSManaged public var title: String?
+    @NSManaged public var url: String?
+ */
+    
+}
