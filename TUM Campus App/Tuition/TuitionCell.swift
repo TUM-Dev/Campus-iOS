fbb37b38412470ce53f86655a9c70375e264e2b1
Mathis Foxius <mathisfoxius@icloud.com>
2019-11-24 02:32:40 +0100

    added UI for Tuitions

---
+++ TUM Campus App/Tuition/TuitionCell.swift
@@ -0,0 +1,16 @@
+//
+//  TuitionCell.swift
+//  TUMCampusApp
+//
+//  Created by Mathis Foxius on 24.11.19.
+//  Copyright © 2019 TUM. All rights reserved.
+//
+
+import UIKit
+
+class TuitionCell: UITableViewCell {
+    @IBOutlet weak var semesterLabel: UILabel!
+    @IBOutlet weak var amountLabel: UILabel!
+    @IBOutlet weak var deadlineLabel: UILabel!
+    
+}
