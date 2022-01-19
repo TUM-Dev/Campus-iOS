aee7a9fafddea811275dba1381c05775c2aab663
August Wittgenstein <august.wittgenstein@tum.de>
2022-01-19 22:42:36 +0100

    MC

--- Campus-iOS/Map/Cafeterias/MenuView.swift
+++ Campus-iOS/Map/Cafeterias/MenuView.swift
@@ -11,18 +11,33 @@ struct MenuView: View {
     @State var title: String
     @State var menu: Menu
     @State var categories: [Category] = []
-    @State var dishNames: [String] = []
-    
+    @State var dishes: [Dish] = []
+    @State private var expanded: [Bool] = []
+
     var body: some View {
         List {
             ForEach(categories, id: \.self) { categoryItem in
                 Section(categoryItem.name) {
-                    ForEach(categoryItem.dishes, id: \.self) { dish in
-                        Text(dish.name)
+                    ForEach(Array(categoryItem.dishes.enumerated()), id: \.1.self) { i, dish in
+                        DisclosureGroup(isExpanded: $expanded[i]) {
+                            HStack {
+                                ForEach(dish.prices.keys.sorted(by: >), id: \.key) { k in
+                                    VStack {
+                                        Text(k)
+                                        //Text(v)
+                                    }
+                                }
+                            }
+                        } label: {
+                            Text(dish.name)
+                                .onTapGesture {
+                                    withAnimation {
+                                        self.expanded[i].toggle()
+                                    }
+                                }
+                        }
                     }
                 }
-                .font(.subheadline)
-                .foregroundColor(.black)
             }
         }
         .navigationTitle(title)
@@ -39,14 +54,14 @@ struct MenuView: View {
         for var c in sortedCategories {
             for d in menu.dishes {
                 if c.name == d.dishType {
+                    expanded.append(false)
                     if !c.dishes.contains(d) {
                         c.dishes.append(d)
+                        expanded.append(false)
                     }
                 }
             }
-            var newCat = Category()
-            newCat.name = c.name
-            newCat.dishes = c.dishes
+            var newCat = Category(name: c.name, dishes: c.dishes)
             categories.append(newCat)
         }
     }
