1dff693539f59d75eda10a60f00996baefdf8387
Philipp, Zagar <philipp.zagar@gmail.com>
2022-03-16 21:59:14 +0100

    Fix view problems on small screen sizes (#417)

--- Campus-iOS/Base/Views/FailedView.swift
+++ Campus-iOS/Base/Views/FailedView.swift
@@ -1,5 +1,5 @@
 //
-//  LoadingView.swift
+//  FailedView.swift
 //  Campus-iOS
 //
 //  Created by Philipp Zagar on 21.12.21.
@@ -11,37 +11,40 @@ struct FailedView: View {
     let errorDescription: String
     
     var body: some View {
-        ZStack (alignment: Alignment(horizontal: .center, vertical: .bottom)) {
-            Image("Error-logo")
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .edgesIgnoringSafeArea(.all)
-            VStack (alignment: .center, spacing: 30) {
-                Text("oh no!")
-                    .font(.title)
-                
-                Text("Someting went wrong.")
-                    .multilineTextAlignment(.center)
-                    .opacity(0.7)
-                
-                Text("Error: \(errorDescription)")
-                    .multilineTextAlignment(.center)
-                    .opacity(0.7)
-                
-                Button(action: {
+        VStack {
+            ZStack (alignment: Alignment(horizontal: .center, vertical: .bottom)) {
+                Image("Error-logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .edgesIgnoringSafeArea(.all)
+                VStack (alignment: .center, spacing: 12) {
+                    Text("oh no!")
+                        .font(.title)
                     
-                }) {
-                    Text("Try Again".uppercased())
-                        .fontWeight(.semibold)
-                        .foregroundColor(.white)
-                        .padding(.vertical)
-                        .padding(.horizontal, 30)
-                        .background(Capsule().foregroundColor(Color("tumBlue")))
+                    Text("Someting went wrong.")
+                        .multilineTextAlignment(.center)
+                        .opacity(0.7)
+                    
+                    Text("Error: \(errorDescription)")
+                        .multilineTextAlignment(.center)
+                        .opacity(0.7)
+                    
+                    Button(action: {
+                        
+                    }) {
+                        Text("Try Again".uppercased())
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                            .padding(.vertical)
+                            .padding(.horizontal, 30)
+                            .background(Capsule().foregroundColor(Color("tumBlue")))
+                    }
                 }
+                .padding(.horizontal, 30)
+                .padding(.bottom, UIScreen.main.bounds.height * 0.1)
             }
-            .padding(.horizontal, 70)
-            .padding(.bottom, UIScreen.main.bounds.height * 0.1)
         }
+        .padding(.bottom, 60)
     }
 }
 
