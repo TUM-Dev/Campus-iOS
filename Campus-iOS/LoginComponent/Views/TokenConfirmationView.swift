7d13c613b8d06b786729a9f5bb336b36d29ed971
David Lin <davidlin1412@yahoo.de>
2022-06-15 23:17:13 +0200

    inital version: looping video without controls

--- Campus-iOS/LoginComponent/Views/TokenConfirmationView.swift
+++ Campus-iOS/LoginComponent/Views/TokenConfirmationView.swift
@@ -6,6 +6,8 @@
 //
 
 import SwiftUI
+import AVKit
+import AVFoundation
 
 struct TokenConfirmationView: View {
     /// Used for the customized back button
@@ -16,105 +18,56 @@ struct TokenConfirmationView: View {
     
     var body: some View {
         ZStack() {
-            GeometryReader { geo in
-                VStack(alignment: .center) {
+            VStack {
+                
+                
+                VStack(alignment: .circleTitleAlignmentGuide) {
                     HStack() {
-                        VStack(alignment: .leading) {
-                            ZStack(content: {})
-                                .frame(width: 42, height: 50)
-                                .background(Color(.systemBackground))
-                            
-                            Spacer().frame(height: 5)
-                            
-                            Text("1")
-                                .frame(width: 42, height: 42, alignment: .center)
-                                .font(.title)
-                                .multilineTextAlignment(.center)
-                                .foregroundColor(Color(white: 1.0))
-                                .background(Color(red: 0.203649, green: 0.35383618, blue: 0.72193307))
-                                .clipShape(Circle())
-                            
-                            Spacer().frame(height: 0)
-                            
-                            ZStack(content: {})
-                                .frame(width: 42, height: 50)
-                                .background(Color(.systemBackground))
-                        }
-                        .frame(width: 42, height: 142)
+                        Text("1")
+                            .frame(width: 42, height: 42, alignment: .center)
+                            .font(.title)
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(Color(white: 1.0))
+                            .background(Color(red: 0.203649, green: 0.35383618, blue: 0.72193307))
+                            .clipShape(Circle())
                         
                         Spacer().frame(width: 20)
                         
                         Text("Check your email and click on the link to confirm your token or visit TUMonline")
                             .font(.body)
                     }
-                    .frame(width: 0.9 * geo.size.width, height: geo.size.height / 5, alignment: .leading)
-                    .offset(y: 30)
                     
                     HStack() {
-                        VStack() {
-                            ZStack(content: {})
-                                .frame(width: 42, height: 50)
-                                .background(Color(.systemBackground))
-                            
-                            Spacer().frame(height: 0)
-                            
-                            Text("2")
-                                .frame(width: 42, height: 42, alignment: .center)
-                                .font(.title)
-                                .multilineTextAlignment(.center)
-                                .foregroundColor(Color(white: 1.0))
-                                .background(Color(red: 0.203649, green: 0.35383618, blue: 0.72193307))
-                                .clipShape(Circle())
-                            
-                            Spacer().frame(height: 0)
-                            
-                            ZStack(content: {})
-                                .frame(width: 42, height: 50)
-                                .background(Color(.systemBackground))
-                        }
-                        .frame(width: 42, height: 142)
+                        Text("2")
+                            .frame(width: 42, height: 42, alignment: .center)
+                            .font(.title)
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(Color(white: 1.0))
+                            .background(Color(red: 0.203649, green: 0.35383618, blue: 0.72193307))
+                            .clipShape(Circle())
                         
                         Spacer().frame(width: 20)
                         
                         Text("Select \"Token-Management\"")
                             .font(.body)
                     }
-                    .frame(width: 0.9 * geo.size.width, height: geo.size.height / 5, alignment: .leading)
-                    .offset(y: 15)
                     
                     HStack() {
-                        VStack(alignment: .leading) {
-                            ZStack(content: {})
-                                .frame(width: 42, height: 50)
-                                .background(Color(.systemBackground))
-                            
-                            Spacer().frame(height: 0)
-                            
-                            Text("3")
-                                .frame(width: 42, height: 42, alignment: .center)
-                                .font(.title)
-                                .multilineTextAlignment(.center)
-                                .foregroundColor(Color(white: 1.0))
-                                .background(Color(red: 0.203649, green: 0.35383618, blue: 0.72193307))
-                                .clipShape(Circle())
-                            
-                            Spacer().frame(height: 0)
-                            
-                            ZStack(content: {})
-                                .frame(width: 42, height: 50)
-                                .background(Color(.systemBackground))
-                        }
+                        Text("3")
+                            .frame(width: 42, height: 42, alignment: .center)
+                            .font(.title)
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(Color(white: 1.0))
+                            .background(Color(red: 0.203649, green: 0.35383618, blue: 0.72193307))
+                            .clipShape(Circle())
                         
                         Spacer().frame(width: 20)
                         
                         Text("Click on the newly created token and enable your desired permissions")
                             .font(.body)
                     }
-                    .frame(width: 0.9 * geo.size.width, height: geo.size.height / 5, alignment: .leading)
                     
                 }
-                .frame(width: geo.size.width, height: 4 * geo.size.height / 5)
-                .offset(y: 50)
                 
                 
                 VStack() {
@@ -135,39 +88,44 @@ struct TokenConfirmationView: View {
                     .alert("Authorization Error", isPresented: self.$viewModel.showTokenAlert) {
                         Button("OK", role: .cancel) {}
                     }
-                    .aspectRatio(contentMode: .fill)
                     .font(.title)
                     .foregroundColor(.white)
                     .background(Color(red: 0.203649, green: 0.35383618, blue: 0.72193307))
                     .cornerRadius(10)
                 }
-                .frame(width: geo.size.width, height: geo.size.height / 5)
-                .offset(x: 0, y: 3.85 * geo.size.height / 5)
+                
+                VStack {
+//                    EnhancedVideoPlayer([Bundle.main.url(forResource: "token-tutorial", withExtension: "mov")!], endAction: .loop)
+                    let videoUrl = Bundle.main
+                        .url(forResource: "token-tutorial", withExtension: "mov")!
+                    PlayerView(videoUrl: videoUrl)
+                }
+                
             }
+            
         }
         .background(Color(.systemBackground))
         ._scrollable()
-        .edgesIgnoringSafeArea(.all)
         .navigationBarBackButtonHidden(true)
         .navigationBarItems(leading:
-            Button(action: {
-                self.showBackButtonAlert = true
-          }) {
+                                Button(action: {
+            self.showBackButtonAlert = true
+        }) {
             HStack {
                 Image(systemName: "arrow.left")
                 Text("Back")
             }
-          }
+        }
         )
         .alert(isPresented: $showBackButtonAlert) {
-              Alert(
+            Alert(
                 title: Text("Are you sure?"),
                 message: Text("Leaving now will invalidate the current token!"),
                 primaryButton: .default(Text("Leave")) {
-                  self.presentationMode.wrappedValue.dismiss()
+                    self.presentationMode.wrappedValue.dismiss()
                 },
                 secondaryButton: .cancel()
-              )
+            )
         }
     }
     
@@ -181,6 +139,140 @@ struct TokenConfirmationView_Previews: PreviewProvider {
     
     static var previews: some View {
         TokenConfirmationView(viewModel: LoginViewModel(model: model))
-            .previewDevice("iPod touch (7th generation)")
+        
     }
+}
+
+extension HorizontalAlignment {
+    /// A custom alignment for image titles.
+    private struct CircleTitleAlignment: AlignmentID {
+        static func defaultValue(in context: ViewDimensions) -> CGFloat {
+            // Default to bottom alignment if no guides are set.
+            context[HorizontalAlignment.leading]
+        }
+    }
+    
+    /// A guide for aligning titles.
+    static let circleTitleAlignmentGuide = HorizontalAlignment(
+        CircleTitleAlignment.self
+    )
+}
+
+
+struct EnhancedVideoPlayer<VideoOverlay: View>: View {
+    @StateObject private var viewModel: ViewModel
+    @ViewBuilder var videoOverlay: () -> VideoOverlay
+    
+    init(_ urls: [URL],
+         endAction: EndAction = .none,
+         @ViewBuilder videoOverlay: @escaping () -> VideoOverlay) {
+        _viewModel = StateObject(wrappedValue: ViewModel(urls: urls, endAction: endAction))
+        self.videoOverlay = videoOverlay
+    }
+    
+    var body: some View {
+        VideoPlayer(player: viewModel.player, videoOverlay: videoOverlay)
+            .onAppear(perform: {
+            viewModel.player.play()
+            })
+    }
+    
+    class ViewModel: ObservableObject {
+        let player: AVQueuePlayer
+        
+        init(urls: [URL], endAction: EndAction) {
+            let playerItems = urls.map { AVPlayerItem(url: $0) }
+            player = AVQueuePlayer(items: playerItems)
+            player.actionAtItemEnd = .none // we'll manually set which video comes next in playback
+            if endAction != .none {
+                // this notification is triggered whenever a player item finishes playing
+                NotificationCenter.default.addObserver(forName: .AVPlayerItemDidPlayToEndTime,
+                                                       object: nil,
+                                                       queue: nil) { [self] notification in
+                    let currentItem = notification.object as? AVPlayerItem
+                    if endAction == .loop,
+                       let currentItem = currentItem {
+                        player.seek(to: .zero) // set the current player item to beginning
+                        player.advanceToNextItem() // move to next video manually
+                        player.insert(currentItem, after: nil) // add it to the end of the queue
+                    }
+                }
+            }
+        }
+    }
+    
+    enum EndAction: Equatable {
+        case none,
+             loop
+    }
+}
+
+extension EnhancedVideoPlayer where VideoOverlay == EmptyView {
+    init(_ urls: [URL], endAction: EndAction) {
+        self.init(urls, endAction: endAction) {
+            EmptyView()
+        }
+    }
+}
+
+struct LoopingVideoPlayer: View {
+    private var playerLooper: AVPlayerLooper
+    private let controller = AVPlayerViewController()
+    
+    init(videoUrl: URL) {
+        let asset = AVAsset(url: videoUrl)
+        let item = AVPlayerItem(asset: asset)
+        
+        let player = AVQueuePlayer()
+        controller.player = player
+        controller.showsPlaybackControls = false
+        playerLooper = AVPlayerLooper(player: player, templateItem: item)
+        
+        player.play()
+    }
+    
+    var body: some View {
+        VideoPlayer(player: controller.player)
+    }
+}
+
+struct PlayerView: UIViewRepresentable {
+    let videoUrl: URL
+
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<PlayerView>) {
+    }
+
+    func makeUIView(context: Context) -> UIView {
+        return PlayerUIView(videoUrl: videoUrl)
+    }
+
+}
+
+class PlayerUIView: UIView {
+    private var playerLooper: AVPlayerLooper
+    private let playerLayer = AVPlayerLayer()
+
+    init(videoUrl: URL) {
+        let asset = AVAsset(url: videoUrl)
+        let item = AVPlayerItem(asset: asset)
+        let player = AVQueuePlayer(playerItem: item)
+        self.playerLayer.player = player
+        
+        self.playerLooper = AVPlayerLooper(player: player, templateItem: item)
+        
+        super.init(frame: .zero)
+        layer.addSublayer(playerLayer)
+        
+        player.play()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        playerLayer.frame = bounds
+    }
+
 }
