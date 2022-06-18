17d66ad61ed5c0f355940577dfed634dab5786a0
David Lin <davidlin1412@yahoo.de>
2022-06-18 22:14:54 +0200

    Fixing the loading inside the places tab. No flicker occur anymore when you switch between cafeterias and study roooms. This was due to the complete reloading of the PanelView which is now reduced to only loading the PanelContentListView, which then loads either the cafeterias (PanelContentCafeteriasListView) or the study rooms (PanelContentStudyGroupsListView). Additionally the LoadingView was adapted to show the text correctly inside the PanelContentView.

---
+++ Campus-iOS/MapComponent/View/PanelContentListView.swift
@@ -0,0 +1,112 @@
+//
+//  PanelContentListView.swift
+//  Campus-iOS
+//
+//  Created by David Lin on 18.06.22.
+//
+
+import SwiftUI
+
+struct PanelContentListView: View {
+    
+    @ObservedObject var vm: MapViewModel
+    @Binding var searchString: String
+    
+    let screenWidth = UIScreen.main.bounds.width
+    let screenHeight = UIScreen.main.bounds.height
+    let position = PanelPosition.bottom
+    
+    var body: some View {
+        
+        switch vm.mode {
+        case .cafeterias:
+            Group {
+                switch vm.cafeteriasState {
+                case .success(_):
+                    VStack {
+                        PanelContentCafeteriasListView(viewModel: vm, searchString: $searchString)
+                    }
+                    .refreshable {
+                        await vm.getCafeteria(forcedRefresh: true)
+                    }
+                case .loading, .na:
+                    ZStack {
+                        Color.white
+                        VStack {
+                            LoadingView(text: "Fetching Canteens", position: .middletop)
+                            Spacer().frame(width: screenWidth, height: screenHeight * (1 - 8.2/10))
+                        }
+                    }
+                case .failed(let error):
+                    FailedView(
+                        errorDescription: error.localizedDescription,
+                        retryClosure: vm.getCafeteria)
+                }
+            }
+            .task {
+                await vm.getCafeteria()
+            }
+            .alert("Error while fetching Cafeterias", isPresented: $vm.hasError, presenting: vm.cafeteriasState) {
+                detail in
+                Button("Retry") {
+                    Task {
+                        await vm.getCafeteria()
+                    }
+                }
+                
+                Button("Cancel", role: .cancel) {}
+            } message: { detail in
+                if case let .failed(error) = detail {
+                    Text(error.localizedDescription)
+                }
+            }
+        case .studyRooms:
+            Group {
+                switch vm.studyRoomsState {
+                case .success(_):
+                    VStack {
+                        PanelContentStudyGroupsListView(viewModel: vm, searchString: $searchString)
+                    }
+                    .refreshable {
+                        await vm.getStudyRoomResponse(forcedRefresh: true)
+                    }
+                case .loading, .na:
+                    ZStack {
+                        Color.white
+                        VStack {
+                            LoadingView(text: "Fetching Study Rooms", position: .middletop)
+                            Spacer().frame(width: screenWidth, height: screenHeight * (1 - 8.2/10))
+                        }
+                    }
+                case .failed(let error):
+                    FailedView(
+                        errorDescription: error.localizedDescription,
+                        retryClosure: vm.getStudyRoomResponse)
+                }
+            }
+            .task {
+                await vm.getStudyRoomResponse()
+            }
+            .alert("Error while fetching Study Rooms", isPresented: $vm.hasError, presenting: vm.studyRoomsState) {
+                detail in
+                Button("Retry") {
+                    Task {
+                        await vm.getStudyRoomResponse()
+                    }
+                }
+                
+                Button("Cancel", role: .cancel) {}
+            } message: { detail in
+                if case let .failed(error) = detail {
+                    Text(error.localizedDescription)
+                }
+            }
+        }
+    }
+}
+
+struct PanelContentListView_Previews: PreviewProvider {
+    static var previews: some View {
+        PanelContentListView(vm: MapViewModel(cafeteriaService: CafeteriasService(), studyRoomsService: StudyRoomsService(), mock: true), searchString: .constant(""))
+    }
+}
