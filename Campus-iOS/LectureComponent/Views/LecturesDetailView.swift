6e4540beaa497eca6f63e710634b32c0449bfaef
August Wittgenstein <august.wittgenstein@tum.de>
2022-07-13 10:21:37 +0200

    DectureDetailsView UI changes

--- Campus-iOS/LectureComponent/Views/LecturesDetailView.swift
+++ Campus-iOS/LectureComponent/Views/LecturesDetailView.swift
@@ -31,6 +31,8 @@ struct LecturesDetailView: View {
                     LectureDetailsDetailedInfoView(lectureDetails: lectureDetails)
                     
                     LectureDetailsLinkView(lectureDetails: lectureDetails)
+                    
+                    Spacer()
                 }
             }
         }
