7b69cd0c86abadd9503c8663f038a6eb1eb3af2f
atharvamathapati <atharva.mathapati@gmail.com>
2022-07-14 22:00:21 +0200

    Fix dismiss button

--- Campus-iOS/MoviesComponent/Views/MovieDetailedView.swift
+++ Campus-iOS/MoviesComponent/Views/MovieDetailedView.swift
@@ -16,82 +16,76 @@ struct MovieDetailedView: View {
     var movie: Movie
     
     var body: some View {
-        NavigationView {
             ScrollView {
-                GeometryReader { geometry in
-                    if let link = self.movie.cover {
-                        AsyncImage(url: link) { image in
-                            switch image {
-                            case .empty:
-                                ProgressView()
-                                    .frame(width: geometry.size.width, height: geometry.size.height)
-
-                            case .success(let image):
-                                if geometry.frame(in: .global).minY <= 0 {
-                                    image
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fill)
+                ZStack {
+                    GeometryReader { geometry in
+                        if let link = self.movie.cover {
+                            AsyncImage(url: link) { image in
+                                switch image {
+                                case .empty:
+                                    ProgressView()
                                         .frame(width: geometry.size.width, height: geometry.size.height)
-                                        .offset(y: geometry.frame(in: .global).minY/9)
-                                        .clipped()
-                                } else {
-                                    image
+                                    
+
+                                case .success(let image):
+                                    if geometry.frame(in: .global).minY <= 0 {
+                                        image
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fill)
+                                            .frame(width: geometry.size.width, height: geometry.size.height)
+                                            .offset(y: geometry.frame(in: .global).minY/9)
+                                            .clipped()
+                                    } else {
+                                        image
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fill)
+                                            .frame(width: geometry.size.width, height: geometry.size.height + geometry.frame(in: .global).minY)
+                                            .clipped()
+                                            .offset(y: -geometry.frame(in: .global).minY)
+                                    }
+                                case .failure:
+                                    Image("movie")
                                         .resizable()
-                                        .aspectRatio(contentMode: .fill)
-                                        .frame(width: geometry.size.width, height: geometry.size.height + geometry.frame(in: .global).minY)
+                                        .frame(minWidth: nil, idealWidth: nil, maxWidth: UIScreen.main.bounds.width, minHeight: nil, idealHeight: nil, maxHeight: UIScreen.main.bounds.height, alignment: .center)
                                         .clipped()
-                                        .offset(y: -geometry.frame(in: .global).minY)
+                                @unknown default:
+                                    // Since the AsyncImagePhase enum isn't frozen,
+                                    // we need to add this currently unused fallback
+                                    // to handle any new cases that might be added
+                                    // in the future:
+                                    EmptyView()
                                 }
-                            case .failure:
-                                Image("movie")
-                                    .resizable()
-                                    .frame(minWidth: nil, idealWidth: nil, maxWidth: UIScreen.main.bounds.width, minHeight: nil, idealHeight: nil, maxHeight: UIScreen.main.bounds.height, alignment: .center)
-                                    .clipped()
-                            @unknown default:
-                                // Since the AsyncImagePhase enum isn't frozen,
-                                // we need to add this currently unused fallback
-                                // to handle any new cases that might be added
-                                // in the future:
-                                EmptyView()
+                            }
+                        } else {
+                            Image("movie")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 120, alignment: .top)
+                        }
+                    }.frame(height: 550)
+                    .edgesIgnoringSafeArea(.bottom)
+                    .navigationBarBackButtonHidden(true)
+                
+                    VStack {
+                        HStack {
+                            Spacer()
+                            Button(action: { self.presentationMode.wrappedValue.dismiss() }) { ZStack {
+                                Circle()
+                                        .frame(width: 30,height:30)
+                                        .foregroundColor(colorScheme == .dark ? .init(UIColor.darkGray) : .init(UIColor.white))
+                                        .shadow(color: Color.black.opacity(0.2), radius: 2)
+
+                                Image(systemName: "xmark")
+                                                .font(.system(size: 15, weight: .bold, design: .rounded))
+                                                .foregroundColor(Color.blue)
+                            }
+                            .padding(8)
+                            .contentShape(Circle())
                             }
                         }
-                    } else {
-                        Image("movie")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(height: 120, alignment: .top)
+                        .padding(.top, 5)
+                        Spacer()
                     }
-                }.frame(height: 550)
-                .edgesIgnoringSafeArea(.bottom)
-                .navigationBarBackButtonHidden(true)
-                .navigationBarItems(trailing: Button(action: { self.presentationMode.wrappedValue.dismiss() }) { ZStack {
-                        Circle()
-                                .frame(width: 30,height:30)
-                                .foregroundColor(colorScheme == .dark ? .init(UIColor.darkGray) : .init(UIColor.white))
-                                .shadow(color: Color.black.opacity(0.2), radius: 2)
-
-                        Image(systemName: "xmark")
-                                        .font(.system(size: 15, weight: .bold, design: .rounded))
-                                        .foregroundColor(Color.blue)
-                    }
-                    .padding(8)
-                    .contentShape(Circle())
-                })
-                .toolbar {
-                    ToolbarItem(placement: .principal) {
-                        RoundedRectangle(cornerRadius: CGFloat(5.0) / 2.0)
-                            .frame(width: 40, height: CGFloat(5.0),alignment: .top)
-                                .foregroundColor(Color.primary.opacity(0.2))
-                                .shadow(color: Color.white, radius: 3)
-                    }
-                }
-                // Remove this 
-                VStack{
-                    Spacer().frame(height: 10)
-                    
-                    RoundedRectangle(cornerRadius: CGFloat(5.0) / 2.0)
-                        .frame(width: 40, height: CGFloat(5.0), alignment: .top)
-                                .foregroundColor(Color.primary.opacity(0.2))
                 }
             
     //        VStack(alignment: .center) {
@@ -121,7 +115,6 @@ struct MovieDetailedView: View {
                 .padding(.horizontal)
                         
             }.edgesIgnoringSafeArea(.top)
-        }.navigationViewStyle(StackNavigationViewStyle())
     }
     
     // TODO: remove when ready OR update accordingly
