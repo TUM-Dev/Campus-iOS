958688e339b09dcf9742ea3007b12078006041d3
August Wittgenstein <august.wittgenstein@tum.de>
2022-09-28 19:21:21 +0200

    Put control of Map Panel into PanelContentView.swift

--- Campus-iOS/MapComponent/View/MapScreenView.swift
+++ Campus-iOS/MapComponent/View/MapScreenView.swift
@@ -14,7 +14,7 @@ struct MapScreenView: View {
     let screenWidth = UIScreen.main.bounds.width
     let screenHeight = UIScreen.main.bounds.width
     
-    @State var panelHeight: CGFloat = 0.0 // Start position of panel is defined in `MapViewModel` with the `panelPos` porperty.
+    var panelHeight: CGFloat = 0.0 // Start position of panel is defined in `MapViewModel` with the `panelPos` porperty.
     
     init(vm: MapViewModel) {
         self._vm = StateObject(wrappedValue: vm)
@@ -29,66 +29,17 @@ struct MapScreenView: View {
                 Spacer()
                 ZStack {
                     VStack {
-                        PanelContentView(vm: self.vm)
+                        PanelContentView(vm: self.vm, panelHeight: panelHeight)
                             .background(Color(UIColor.systemBackground))
                             .cornerRadius(10, corners: [.topRight, .topLeft])
                             .shadow(radius: 10)
                             .frame(height: panelHeight, alignment: .bottomTrailing)
-                            .task(id: vm.panelPos) {
-                                if panelHeight != vm.panelPos.rawValue {
-                                    withAnimation(.interpolatingSpring(stiffness: 300.0, damping: 30.0, initialVelocity: 10.0)) {
-                                        panelHeight = vm.panelPos.rawValue
-                                        print(panelHeight)
-                                    }
-                                }
-                            }
-                    }
-                    
-                    VStack {
-//                        Spacer()
-                        let dragAreaHeight = PanelHeight.top*0.04
-                        
-                        Rectangle().foregroundColor(.clear)
-                        .contentShape(Rectangle())
-                        .cornerRadius(10, corners: [.topLeft, .topRight])
-                        .frame(height: dragAreaHeight, alignment: .top)
-                        .gesture(
-                            DragGesture()
-                                .onChanged { value in
-                                    guard !vm.lockPanel else { return }
-                                    if let newPanelHeight = check(panelHeight - value.translation.height) {
-                                        panelHeight = newPanelHeight
-                                    }
-                                }
-                                .onEnded { _ in
-                                    withAnimation(.interpolatingSpring(stiffness: 300.0, damping: 30.0, initialVelocity: 10.0)) {
-                                        snapPanel(from: panelHeight)
-                                    }
-                                }
-                        )
-                        Spacer().frame(height: min(max(panelHeight-dragAreaHeight,0), PanelPos.top.rawValue))
                     }
                 }
             }
         }.edgesIgnoringSafeArea(.top)
             .navigationTitle("Map")
             .navigationBarHidden(true)
-    }
-    
-    func check(_ height: CGFloat) -> CGFloat? {
-        return height <= PanelHeight.top && height >= PanelHeight.bottom ? height : nil
-    }
-    
-    func snapPanel(from height: CGFloat) {
-        print(height)
-        let snapHeights = [PanelPos.top, PanelPos.middle, PanelPos.bottom]
-        
-        vm.panelPos = closestMatch(values: snapHeights, inputValue: height)
-        panelHeight = vm.panelPos.rawValue
-    }
-    
-    func closestMatch(values: [PanelPos], inputValue: CGFloat) -> PanelPos {
-        return (values.reduce(values[0]) { abs($0.rawValue-inputValue) < abs($1.rawValue-inputValue) ? $0 : $1 })
     }
 }
 
--- Campus-iOS/MapComponent/View/PanelContentView.swift
+++ Campus-iOS/MapComponent/View/PanelContentView.swift
@@ -22,113 +22,149 @@ struct PanelContentView: View {
     // in .onDisappear mitigates the issue.
     @State private var panelContentDidAppear = false
     
+    @State var panelHeight: CGFloat = 0.0
+    
     private let handleThickness = CGFloat(0)
     
     var body: some View {
-        VStack {
-            
-            Spacer().frame(height: 10)
-            
-            RoundedRectangle(cornerRadius: CGFloat(5.0) / 2.0)
-                .frame(width: 40, height: CGFloat(5.0))
-                .foregroundColor(Color.primary.opacity(0.2))
-            
-            if vm.selectedCafeteria != nil {
-                CafeteriaView(selectedCanteen: $vm.selectedCafeteria)
-                if let viewModel = mealPlanViewModel {
-                    MealPlanView(viewModel: viewModel)
-                }
+        ZStack {
+            VStack {
                 
-            } else if vm.selectedStudyGroup != nil {
-                StudyRoomGroupView(
-                    selectedGroup: $vm.selectedStudyGroup,
-                    rooms: vm.selectedStudyGroup?.getRooms(allRooms: vm.studyRoomsResponse.rooms ?? [StudyRoom]()) ?? [StudyRoom]()
-                )
-            }
-            else {
-                HStack {
-                    Spacer()
-                    
-                    Button (action: {
-                        vm.zoomOnUser = true
-                        vm.panelPos = .middle
-                    }) {
-                        Image(systemName: "location")
-                            .font(.title2)
-                            .foregroundColor(Color(UIColor.tumBlue))
+                Spacer().frame(height: 10)
+                
+                RoundedRectangle(cornerRadius: CGFloat(5.0) / 2.0)
+                    .frame(width: 40, height: CGFloat(5.0))
+                    .foregroundColor(Color.primary.opacity(0.2))
+                
+                if vm.selectedCafeteria != nil {
+                    CafeteriaView(selectedCanteen: $vm.selectedCafeteria)
+                    if let viewModel = mealPlanViewModel {
+                        MealPlanView(viewModel: viewModel)
                     }
                     
-                    Spacer()
-                    
-                    PanelSearchBarView(vm: self.vm, searchString: $searchString)
-                    
-                    Spacer()
-                    
+                } else if vm.selectedStudyGroup != nil {
+                    StudyRoomGroupView(
+                        selectedGroup: $vm.selectedStudyGroup,
+                        rooms: vm.selectedStudyGroup?.getRooms(allRooms: vm.studyRoomsResponse.rooms ?? [StudyRoom]()) ?? [StudyRoom]()
+                    )
+                }
+                else {
                     HStack {
-                        Picker("Places", selection: $vm.mode) {
-                            ForEach(MapMode.allCases, id: \.self) {
-                                switch $0 {
-                                case .cafeterias:
-                                    Image(systemName: "fork.knife")
-                                case .studyRooms:
-                                    Image(systemName: "book.fill")
+                        Spacer()
+                        
+                        Button (action: {
+                            vm.zoomOnUser = true
+                            vm.panelPos = .middle
+                        }) {
+                            Image(systemName: "location")
+                                .font(.title2)
+                                .foregroundColor(Color(UIColor.tumBlue))
+                        }
+                        
+                        Spacer()
+                        
+                        PanelSearchBarView(vm: self.vm, searchString: $searchString)
+                        
+                        Spacer()
+                        
+                        HStack {
+                            Picker("Places", selection: $vm.mode) {
+                                ForEach(MapMode.allCases, id: \.self) {
+                                    switch $0 {
+                                    case .cafeterias:
+                                        Image(systemName: "fork.knife")
+                                    case .studyRooms:
+                                        Image(systemName: "book.fill")
+                                    }
                                 }
                             }
+                            .pickerStyle(.segmented)
+                            .onAppear {
+                                UISegmentedControl.appearance().backgroundColor = UIColor.systemBlue.withAlphaComponent(0.2)
+                                UISegmentedControl.appearance()
+                                    .selectedSegmentTintColor = .tumBlue
+                                UISegmentedControl.appearance()
+                                    .setTitleTextAttributes([.foregroundColor: UIColor.white], for: .selected)
+                                UISegmentedControl.appearance()
+                                    .setTitleTextAttributes([.foregroundColor: UIColor.useForStyle(dark: UIColor(red: 28/255, green: 171/255, blue: 246/255, alpha: 1), white: UIColor(red: 34/255, green: 126/255, blue: 177/255, alpha: 1))], for: .normal)
+                            }
                         }
-                        .pickerStyle(.segmented)
-                        .onAppear {
-                            UISegmentedControl.appearance().backgroundColor = UIColor.systemBlue.withAlphaComponent(0.2)
-                            UISegmentedControl.appearance()
-                                .selectedSegmentTintColor = .tumBlue
-                            UISegmentedControl.appearance()
-                                .setTitleTextAttributes([.foregroundColor: UIColor.white], for: .selected)
-                            UISegmentedControl.appearance()
-                                .setTitleTextAttributes([.foregroundColor: UIColor.useForStyle(dark: UIColor(red: 28/255, green: 171/255, blue: 246/255, alpha: 1), white: UIColor(red: 34/255, green: 126/255, blue: 177/255, alpha: 1))], for: .normal)
+                        .frame(width: 100)
+                        .onChange(of: vm.mode) { newMode in
+                            if newMode == .cafeterias {
+                                studyRoomsData.didExitView()
+                                cafeteriasData.visitView(view: .cafeterias)
+                            } else if newMode == .studyRooms {
+                                cafeteriasData.didExitView()
+                                studyRoomsData.visitView(view: .studyRooms)
+                            }
+                            vm.setAnnotations = true
                         }
-                    }
-                    .frame(width: 100)
-                    .onChange(of: vm.mode) { newMode in
-                        if newMode == .cafeterias {
-                            studyRoomsData.didExitView()
-                            cafeteriasData.visitView(view: .cafeterias)
-                        } else if newMode == .studyRooms {
-                            cafeteriasData.didExitView()
-                            studyRoomsData.visitView(view: .studyRooms)
-                        }
-                        vm.setAnnotations = true
-                    }
-            
-                    Spacer().frame(width: 0.25 * UIScreen.main.bounds.width/10, height: 50)
-                }
                 
-                Spacer()
-                
-                PanelContentListView(vm: self.vm, searchString: $searchString)
-                    .task {
-                        
-                        panelContentDidAppear = true
+                        Spacer().frame(width: 0.25 * UIScreen.main.bounds.width/10, height: 50)
+                    }
+                    
+                    Spacer()
+                    
+                    PanelContentListView(vm: self.vm, searchString: $searchString)
+                        .task {
+                            
+                            panelContentDidAppear = true
 
-                        if currentView() == .cafeterias {
-                            cafeteriasData.visitView(view: .cafeterias)
-                        } else if currentView() == .studyRooms {
-                            studyRoomsData.visitView(view: .studyRooms)
+                            if currentView() == .cafeterias {
+                                cafeteriasData.visitView(view: .cafeterias)
+                            } else if currentView() == .studyRooms {
+                                studyRoomsData.visitView(view: .studyRooms)
+                            }
                         }
-                    }
-                    .onDisappear {
-                        
-                        if !panelContentDidAppear { return }
-                        
-                        if currentView() == .cafeterias {
-                            cafeteriasData.didExitView()
-                        } else if currentView() == .studyRooms {
-                            studyRoomsData.didExitView()
+                        .onDisappear {
+                            
+                            if !panelContentDidAppear { return }
+                            
+                            if currentView() == .cafeterias {
+                                cafeteriasData.didExitView()
+                            } else if currentView() == .studyRooms {
+                                studyRoomsData.didExitView()
+                            }
                         }
-                    }
+                }
             }
-        }
-        .onChange(of: vm.selectedCafeteria) { optionalCafeteria in
-            if let cafeteria = optionalCafeteria {
-                mealPlanViewModel = MealPlanViewModel(cafeteria: cafeteria)
+            .onChange(of: vm.selectedCafeteria) { optionalCafeteria in
+                if let cafeteria = optionalCafeteria {
+                    mealPlanViewModel = MealPlanViewModel(cafeteria: cafeteria)
+                }
+            }
+            .task(id: vm.panelPos) {
+                if panelHeight != vm.panelPos.rawValue {
+                    withAnimation(.interpolatingSpring(stiffness: 300.0, damping: 30.0, initialVelocity: 10.0)) {
+                        panelHeight = vm.panelPos.rawValue
+                        print(panelHeight)
+                    }
+                }
+            }
+            
+            VStack {
+                let dragAreaHeight = PanelHeight.top*0.04
+                
+                Rectangle().foregroundColor(.clear)
+                .contentShape(Rectangle())
+                .cornerRadius(10, corners: [.topLeft, .topRight])
+                .frame(height: dragAreaHeight, alignment: .top)
+                .gesture(
+                    DragGesture()
+                        .onChanged { value in
+                            guard !vm.lockPanel else { return }
+                            if let newPanelHeight = check(panelHeight - value.translation.height) {
+                                panelHeight = newPanelHeight
+                            }
+                        }
+                        .onEnded { _ in
+                            withAnimation(.interpolatingSpring(stiffness: 300.0, damping: 30.0, initialVelocity: 10.0)) {
+                                snapPanel(from: panelHeight)
+                            }
+                        }
+                )
+                Spacer().frame(height: min(max(panelHeight-dragAreaHeight,0), PanelPos.top.rawValue))
             }
         }
     }
@@ -138,6 +174,22 @@ struct PanelContentView: View {
         case MapMode.studyRooms: return .studyRooms
         case MapMode.cafeterias: return .cafeterias
         }
+    }
+    
+    func check(_ height: CGFloat) -> CGFloat? {
+        return height <= PanelHeight.top && height >= PanelHeight.bottom ? height : nil
+    }
+    
+    func snapPanel(from height: CGFloat) {
+        print(height)
+        let snapHeights = [PanelPos.top, PanelPos.middle, PanelPos.bottom]
+        
+        vm.panelPos = closestMatch(values: snapHeights, inputValue: height)
+        panelHeight = vm.panelPos.rawValue
+    }
+    
+    func closestMatch(values: [PanelPos], inputValue: CGFloat) -> PanelPos {
+        return (values.reduce(values[0]) { abs($0.rawValue-inputValue) < abs($1.rawValue-inputValue) ? $0 : $1 })
     }
 }
 
