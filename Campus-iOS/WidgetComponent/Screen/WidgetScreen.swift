f43351fd954fc344fe3fb6ff897274fabc14218c
Robyn Kölle <robyn.koelle@tum.de>
2022-10-08 22:29:55 +0200

    Use spatio-temporal strategy for widget recommendations

--- Campus-iOS/WidgetComponent/Screen/WidgetScreen.swift
+++ Campus-iOS/WidgetComponent/Screen/WidgetScreen.swift
@@ -12,7 +12,7 @@ struct WidgetScreen: View {
     @StateObject private var recommender: WidgetRecommender
     
     init(model: Model) {
-        self._recommender = StateObject(wrappedValue: WidgetRecommender(strategy: AnalyticsStrategy(), model: model))
+        self._recommender = StateObject(wrappedValue: WidgetRecommender(strategy: SpatioTemporalStrategy(), model: model))
     }
     
     var body: some View {
