8de5ce5571d933bc16ed547ada605ea7226a97ec
Robyn Kölle <robyn.koelle@tum.de>
2022-10-08 22:14:01 +0200

    Arrange widgets dynamically on widget screen

--- Campus-iOS/WidgetComponent/Screen/WidgetScreen.swift
+++ Campus-iOS/WidgetComponent/Screen/WidgetScreen.swift
@@ -22,21 +22,60 @@ struct WidgetScreen: View {
             case .loading:
                 ProgressView()
             case .success:
-                ScrollView {
-                    
-                    // TODO: use a flexible grid.
-                    VStack {
-                        ForEach(recommender.recommendations, id: \.widget) { recommendation in
-                            recommender.getWidget(for: recommendation.widget, size: recommendation.size())
-                        }
+                GeometryReader { geometry in
+                    ScrollView {
+                        self.generateContent(in: geometry, views: recommender.recommendations.map { recommender.getWidget(for: $0.widget, size: $0.size()) })
+                            .frame(maxWidth: .infinity)
                     }
-                    .frame(maxWidth: .infinity)
-                    .padding()
                 }
             }
         }
         .task {
             try? await recommender.fetchRecommendations()
+        }
+    }
+    
+    // Source: https://stackoverflow.com/a/58876712
+    private func generateContent<T: View>(in g: GeometryProxy, views: [T]) -> some View {
+        
+        var width = CGFloat.zero
+        var height = CGFloat.zero
+        var previousHeight = CGFloat.zero
+        
+        return ZStack(alignment: .topLeading) {
+            ForEach(0..<views.count, id: \.self) { i in
+                views[i]
+                    .padding([.horizontal, .vertical], WidgetSize.padding)
+                    .alignmentGuide(.leading) { d in
+                        
+                        if (abs(width - d.width) > g.size.width) {
+                            width = 0
+                            height -= previousHeight
+                        }
+                        
+                        let result = width
+                        
+                        if i == views.count - 1 {
+                            width = 0
+                        } else {
+                            width -= d.width
+                        }
+                        
+                        previousHeight = d.height
+                        
+                        return result
+                    }
+                    .alignmentGuide(.top) { d in
+                        
+                        let result = height
+                        
+                        if i == views.count - 1 {
+                            height = 0
+                        }
+                        
+                        return result
+                    }
+            }
         }
     }
 }
