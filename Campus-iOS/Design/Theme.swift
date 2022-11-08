270e38f3f8a6f05ebe7d60fac44fd1756c7f11de
Tim <62808178+timothymaxwellsummers@users.noreply.github.com>
2022-11-08 18:18:38 +0100

    returned to extensions

--- Campus-iOS/Design/Theme.swift
+++ Campus-iOS/Design/Theme.swift
@@ -8,15 +8,13 @@
 import Foundation
 import SwiftUI
 
-class Theme {
-    struct Colors {
-        static let tumBrand = Color("tumBrand")
-        static let background = Color("background")
-        static let contrastBackground = Color("contrastBackground")
-    }
+extension Color {
+    static let tumBrand = Color("tumBrand")
+    static let background = Color("background")
+    static let contrastBackground = Color("contrastBackground")
+}
    
-    struct Fonts {
-        static let body = Font.custom("Roboto", size: 12)
-        static let title = Font.custom("Roboto", size: 12)
-    }
+extension Font {
+    static let body = Font.custom("Roboto", size: 12)
+    static let title = Font.custom("Roboto", size: 12)
 }
