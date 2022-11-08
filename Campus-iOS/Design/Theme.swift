b897bd2fecda7433bcbeb89add7cdc83bc5c4e0d
Tim <62808178+timothymaxwellsummers@users.noreply.github.com>
2022-11-08 16:32:50 +0100

    Solved Xcode issue

---
+++ Campus-iOS/Design/Theme.swift
@@ -0,0 +1,22 @@
+//
+//  Theme.swift
+//  Campus-iOS
+//
+//  Created by Timothy Summers on 08.11.22.
+//
+
+import Foundation
+import SwiftUI
+
+class Theme {
+    struct Colors {
+        static let tumBrand = Color("tumBrand")
+        static let background = Color("background")
+        static let contrastBackground = Color("contrastBackground")
+    }
+   
+    struct Fonts {
+        static let body = Font.custom("Roboto", size: 12)
+        static let title = Font.custom("Roboto", size: 12)
+    }
+}
