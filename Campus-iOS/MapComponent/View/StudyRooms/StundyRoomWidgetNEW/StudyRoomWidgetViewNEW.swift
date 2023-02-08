f8bdef37451b9b9c751e662f04c10fd0ee66d1e9
Tim <62808178+timothymaxwellsummers@users.noreply.github.com>
2023-02-08 14:34:04 +0100

    Added new StudyRoomWidget

--- Campus-iOS/MapComponent/View/StudyRooms/StundyRoomWidgetNEW/StudyRoomWidgetViewNEW.swift
+++ Campus-iOS/MapComponent/View/StudyRooms/StundyRoomWidgetNEW/StudyRoomWidgetViewNEW.swift
@@ -12,9 +12,38 @@ struct StudyRoomWidgetViewNEW: View {
     @StateObject var studyRoomWidgetVM : StudyRoomWidgetViewModel
     
     var body: some View {
-        if let studyGroup = studyRoomWidgetVM.studyGroup {
-            Text(studyGroup.name!)
-                .sectionStyle()
+        NavigationLink(destination: StudyRoomGroupView(
+            vm: MapViewModel(cafeteriaService: CafeteriasService(), studyRoomsService: StudyRoomsService()),
+            selectedGroup: $studyRoomWidgetVM.studyGroup,
+            rooms: studyRoomWidgetVM.rooms ?? [],
+            canDismiss: false
+        )) {
+            if let studyGroup = self.studyRoomWidgetVM.studyGroup,
+               let rooms = studyRoomWidgetVM.rooms {
+                Label {
+                    HStack {
+                        Text(studyGroup.name!).foregroundColor(Color.primaryText)
+                        if let freeRooms = rooms.filter{ $0.isAvailable() }.count,
+                        freeRooms > 0 {
+                            Spacer()
+                            Text("\(freeRooms) rooms free").foregroundColor(.green)
+                        } else {
+                            Spacer()
+                            Text("No rooms free").foregroundColor(.red)
+                        }
+                        Image(systemName: "chevron.right").foregroundColor(Color.primaryText)
+                    }
+                } icon: {
+                    Image(systemName: "book").foregroundColor(Color.primaryText)
+                }
+                .padding(.vertical, 15)
+                .padding(.horizontal)
+            } else {
+                
+            }
         }
+        .frame(width: Size.cardWidth)
+        .background(Color.secondaryBackground)
+        .clipShape(RoundedRectangle(cornerRadius: Radius.regular))
     }
 }
