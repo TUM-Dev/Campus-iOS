b3b5479b2e1665c8da5fb2ac8517b629768f57c2
David Lin <davidlin1412@yahoo.de>
2023-02-12 15:41:40 +0100

    Make the expanding button work in the PersonSearchResultView

--- Campus-iOS/SearchComponent/Views/SearchResultViews/PersonSearchResultView.swift
+++ Campus-iOS/SearchComponent/Views/SearchResultViews/PersonSearchResultView.swift
@@ -12,15 +12,70 @@ struct PersonSearchResultView: View {
     @StateObject var vm : PersonSearchResultViewModel
     @Binding var query: String
     
+    @State var size: ResultSize = .small
+    
+    var results: [Person] {
+        switch size {
+        case .small:
+            return Array(vm.results.prefix(3))
+        case .big:
+            return Array(vm.results.prefix(10))
+        }
+    }
+    
     var body: some View {
         ZStack {
             Color.white
-            ScrollView {
-                Text("\(vm.results.count)")
-                ForEach(vm.results, id: \.id) { result in
-                    Text(result.fullName)
+            VStack {
+                VStack {
+                    ZStack {
+                        Text("Person Search")
+                            .fontWeight(.bold)
+                            .font(.title)
+                        HStack {
+                            Spacer()
+                            Button {
+                                switch size {
+                                case .big:
+                                    withAnimation {
+                                        self.size = .small
+                                    }
+                                case .small:
+                                    withAnimation {
+                                        self.size = .big
+                                    }
+                                }
+                            } label: {
+                                if self.size == .small {
+                                    Image(systemName: "arrow.up.left.and.arrow.down.right")
+                                        .padding()
+                                } else {
+                                    Image(systemName: "arrow.down.right.and.arrow.up.left")
+                                        .padding()
+                                }
+                            }
+                        }
+                    }
                 }
-            }
+                ScrollView {
+                    ForEach(vm.results, id: \.id) { result in
+                        VStack(alignment: .leading) {
+                            NavigationLink(
+                                destination: PersonDetailedView(withPerson: result)
+                                    .navigationBarTitleDisplayMode(.inline)
+                            ) {
+                                HStack {
+                                    Text(result.fullName)
+                                    Spacer()
+                                    Image(systemName: "info.circle")
+                                        .foregroundColor(.tumBlue)
+                                }
+                            }.buttonStyle(.plain)
+                            Divider()
+                        }
+                    }
+                }
+            }.padding()
         }.onChange(of: query) { newQuery in
             Task {
                 await vm.personSearch(for: newQuery)
