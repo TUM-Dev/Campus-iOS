7b44f50c6b3fccfc764a2595954d4a6f140d294f
David Lin <davidlin1412@yahoo.de>
2023-05-06 10:18:48 +0200

    Extract ExpandIcon in own file

--- Campus-iOS/SearchComponent/Views/SearchResultViews/Movie/MovieSearchResultView.swift
+++ Campus-iOS/SearchComponent/Views/SearchResultViews/Movie/MovieSearchResultView.swift
@@ -7,34 +7,6 @@
 
 import SwiftUI
 
-struct ExpandIcon: View {
-    @Binding var size: ResultSize
-    
-    var body: some View {
-        HStack(alignment: .center) {
-            Spacer()
-            Button {
-                withAnimation {
-                    switch size {
-                    case .big:
-                        self.size = .small
-                    case .small:
-                        self.size = .big
-                    }
-                }
-            } label: {
-                if self.size == .small {
-                    Image(systemName: "arrow.up.left.and.arrow.down.right")
-                        .padding()
-                } else {
-                    Image(systemName: "arrow.down.right.and.arrow.up.left")
-                        .padding()
-                }
-            }
-        }
-    }
-}
-
 struct MovieSearchResultView: View {
     let allResults: [(movie: Movie, distance: Distances)]
     
