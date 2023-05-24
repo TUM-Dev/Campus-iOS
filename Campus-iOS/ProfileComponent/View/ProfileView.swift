700764fd3d3930f7ad5b9e7d2d56234700af0dfe
August Wittgenstein <august.wittgenstein@tum.de>
2023-05-24 19:27:23 +0200

    Fix Crash: If links at the bottom of the profile view are clicked, the webview sheet opens twice, hence the app crashes

--- Campus-iOS/ProfileComponent/View/ProfileView.swift
+++ Campus-iOS/ProfileComponent/View/ProfileView.swift
@@ -16,10 +16,12 @@ struct ProfileView: View {
     @Environment(\.colorScheme) var colorScheme
     @Environment(\.dismiss) var dismiss
     @State var isWebViewShowed = false
-    @State var selectedLink: URL? = nil
+    @State private var showSheet: Bool = false
+    @State private var url: URL?
     
     init(model: Model) {
         self._vm = StateObject(wrappedValue: ProfileViewModel(model: model, service: ProfileService()))
+        self.url = .init(string: "https://google.com")
     }
     
     var body: some View {
@@ -113,15 +115,18 @@ struct ProfileView: View {
                 Section("GET IN CONTACT") {
                     if self.useBuildInWebView {
                         Button("Join Beta") {
-                            self.selectedLink = URL(string: "https://testflight.apple.com/join/4Ddi6f2f")
+                            self.url = URL(string: "https://testflight.apple.com/join/4Ddi6f2f")!
+                            showSheet = true
                         }
                         
                         Button("TUM Dev on Github") {
-                            self.selectedLink = URL(string: "https://github.com/TUM-Dev")
+                            self.url = URL(string: "https://github.com/TUM-Dev")!
+                            showSheet = true
                         }
                         
                         Button("TUM Dev Website") {
-                            self.selectedLink = URL(string: "https://tum.app")
+                            self.url = URL(string: "https://tum.app")!
+                            showSheet = true
                         }
                     } else {
                         Link(LocalizedStringKey("Join Beta"), destination: URL(string: "https://testflight.apple.com/join/4Ddi6f2f")!)
@@ -204,9 +209,8 @@ struct ProfileView: View {
                 }
 
             }
-            .sheet(item: $selectedLink) { selectedLink in
-                // This if clause is needed since after logout via the ProfileView an issue occured. This fixes the weird behaviour.
-                SFSafariViewWrapper(url: selectedLink)
+            .sheet(isPresented: $showSheet) {
+                if showSheet { SFSafariViewWrapper(url: url!) }
             }
         }.task {
             await vm.getProfile(forcedRefresh: false)
@@ -254,214 +258,3 @@ struct ProfileCell: View {
         .padding(.vertical, 6)
     }
 }
-
-//struct ProfileView: View {
-//    @State var showActionSheet = false
-//    @ObservedObject var model: Model
-//    @AppStorage("useBuildInWebView") var useBuildInWebView: Bool = true
-//    @AppStorage("calendarWeekDays") var calendarWeekDays: Int = 7
-//    @Environment(\.colorScheme) var colorScheme
-//    @State var isWebViewShowed = false
-//    @State var selectedLink: URL? = nil
-//
-//    var body: some View {
-//
-//        NavigationView {
-//
-//            List {
-//                NavigationLink(destination: PersonDetailedScreen(model: self.model, profile: self.model.profile.profile ?? ProfileViewModel.defaultProfile)) {
-//                    HStack(spacing: 24) {
-//                        self.model.profile.profileImage
-//                            .resizable()
-//                            .clipShape(Circle())
-//                            .aspectRatio(contentMode: .fill)
-//                            .frame(width: 75, height: 75)
-//                            .foregroundColor(Color(.secondaryLabel))
-//
-//                        VStack(alignment: .leading) {
-//                            if self.model.isUserAuthenticated {
-//                                Text(self.model.profile.profile?.fullName ?? "TUM Student")
-//                                    .font(.title2)
-//                            } else {
-//                                Text("Not logged in")
-//                                    .font(.title2)
-//                            }
-//
-//                            Text(self.model.profile.profile?.tumID ?? "TUM ID")
-//                                .font(.subheadline)
-//                                .foregroundColor(.gray)
-//                        }
-//                    }
-//                    .padding(.vertical, 6)
-//                }.disabled(!self.model.isUserAuthenticated)
-//
-//                ProfileMyTumSection()
-//
-//                Section("GENERAL") {
-//                    NavigationLink(destination: TUMSexyView().navigationBarTitle(Text("Useful Links"))) {
-//                        Label("TUM.sexy", systemImage: "heart")
-//                    }
-//
-//                    NavigationLink(
-//                        destination: RoomFinderView(model: self.model)
-//                            .navigationTitle(Text("Roomfinder"))
-//                            .navigationBarTitleDisplayMode(.large)
-//                    ) {
-//                        Label("Roomfinder", systemImage: "rectangle.portrait.arrowtriangle.2.inward")
-//                    }
-//
-//                    NavigationLink(destination: NewsView(viewModel: NewsViewModel())
-//                                    .navigationBarTitle(Text("News"))
-//                                    .navigationBarTitleDisplayMode(.large)
-//                    ) {
-//                        Label("News", systemImage: "newspaper")
-//                    }
-//
-//                    NavigationLink(destination: MoviesView()
-//                                    .navigationBarTitle(Text("Movies"))
-//                                    .navigationBarTitleDisplayMode(.large)
-//                    ) {
-//                        Label("Movies", systemImage: "film")
-//                    }
-//
-//                    NavigationLink(destination: TokenPermissionsView(viewModel: TokenPermissionsViewModel(model: self.model), dismissWhenDone: true).navigationBarTitle("Check Permissions")) {
-//                        if self.model.isUserAuthenticated {
-//                            Label("Token Permissions", systemImage: "key")
-//                        } else {
-//                            Label("Token Permissions (You are logged out)", systemImage: "key")
-//                        }
-//
-//                    }.disabled(!self.model.isUserAuthenticated)
-//                }
-//
-//                Section() {
-//                    VStack {
-//                        Toggle("Use build-in Web View", isOn: $useBuildInWebView)
-//                    }
-//                }
-//
-//                Section() {
-//                    HStack {
-//
-//                        if #unavailable(iOS 16.0) {
-//                            Text("Calendar days in week mode")
-//                                .foregroundColor(Color(.label))
-//                            Spacer()
-//                        }
-//                        Picker(selection: $calendarWeekDays, label: Text("Calendar days in week mode").foregroundColor(Color(.label))) {
-//                            ForEach(2..<8) { number in
-//                                Text("\(number)")
-//                                    .tag(number)
-//                            }
-//                        }
-//                    }
-//                    .pickerStyle(MenuPickerStyle())
-//                    .foregroundColor(.black)
-//                }
-//
-//                Section("GET IN CONTACT") {
-//                    if self.useBuildInWebView {
-//                        Button("Join Beta") {
-//                            self.selectedLink = URL(string: "https://testflight.apple.com/join/4Ddi6f2f")
-//                        }
-//
-//                        Button("TUM Dev on Github") {
-//                            self.selectedLink = URL(string: "https://github.com/TUM-Dev")
-//                        }
-//
-//                        Button("TUM Dev Website") {
-//                            self.selectedLink = URL(string: "https://tum.app")
-//                        }
-//                    } else {
-//                        Link(LocalizedStringKey("Join Beta"), destination: URL(string: "https://testflight.apple.com/join/4Ddi6f2f")!)
-//
-//                        Link(LocalizedStringKey("TUM Dev on Github"), destination: URL(string: "https://github.com/TUM-Dev")!)
-//
-//                        Link("TUM Dev Website", destination: URL(string: "https://tum.app")!)
-//                    }
-//
-//                    Button("Feedback") {
-//                        let mailToString = "mailto:app@tum.de?subject=[IOS]&body=Hello I have an issue...".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
-//                        let mailToUrl = URL(string: mailToString!)!
-//                        if UIApplication.shared.canOpenURL(mailToUrl) {
-//                                UIApplication.shared.open(mailToUrl, options: [:])
-//                        }
-//                    }
-//                }
-//
-//                Section() {
-//                    HStack(alignment: .bottom) {
-//                        Spacer()
-//                        if model.isUserAuthenticated {
-//                            Button(action: {
-//                                model.logout()
-//                            }) {
-//                                Text("Sign Out").foregroundColor(.red)
-//                            }
-//                        } else {
-//                            Button(action: {
-//                                model.isLoginSheetPresented = true
-//                            }) {
-//                                Text("Sign In").foregroundColor(.green)
-//                            }
-//                        }
-//                        Spacer()
-//                    }
-//                }
-//
-//                Section() {
-//                    var i = 0
-//                    Button(action: {
-//                        i += 1
-//                        if i == 5 {
-//                            i = 0
-//                            self.showActionSheet = true
-//                        }
-//                    }) {
-//                        HStack {
-//                            Spacer()
-//                            Text("Version 4.1").foregroundColor(colorScheme == .dark ? .init(UIColor.lightGray) : .init(UIColor.darkGray))
-//                            Spacer()
-//                        }
-//                    }
-//                }
-//                .actionSheet(isPresented: self.$showActionSheet) {
-//                    //ActionSheet(title: Text("Choose Speaker"), buttons: self.actionSheetButtons)
-//                    ActionSheet(title: Text("Change App icon"), message: Text("Select a new design"), buttons: [
-//                        .default(Text("Default 🎓")) { UIApplication.shared.setAlternateIconName(nil) },
-//                        .default(Text("Inverted 🔄")) { UIApplication.shared.setAlternateIconName("inverted") },
-//                        .default(Text("Pride 🏳️‍🌈")) { UIApplication.shared.setAlternateIconName("pride") },
-//                        .default(Text("3D 📐")) { UIApplication.shared.setAlternateIconName("3D") },
-//                        .default(Text("Outline 🖍")) { UIApplication.shared.setAlternateIconName("outline") },
-//                        .cancel()
-//                    ])
-//                }
-//                .listRowBackground(Color.clear)
-//            }
-//            .sheet(isPresented: $model.isLoginSheetPresented) {
-//                NavigationView {
-//                    LoginView(model: model)
-//                }
-//            }
-//            .navigationTitle("Profile")
-//            .navigationBarTitleDisplayMode(.inline)
-//            .toolbar {
-//                Button(action: {model.showProfile.toggle()}) {
-//                    Text("Done").bold()
-//                }
-//            }
-//            .sheet(item: $selectedLink) { selectedLink in
-//                if let link = selectedLink {
-//                    SFSafariViewWrapper(url: link)
-//                }
-//            }
-//        }
-//    }
-//}
-
-//struct ProfileView_Previews: PreviewProvider {
-//    
-//    static var previews: some View {
-//        ProfileView(model: MockModel()).environmentObject(MockModel())
-//    }
-//}
