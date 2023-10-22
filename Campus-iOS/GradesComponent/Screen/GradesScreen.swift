c3d10cb792c96d4b8e82243b72276ea7a29251b2
mkandil <mkandil@synapse-analytics.io>
2023-10-23 00:51:03 +0200

    feat: add EmptyGradesView() in grades view

--- Campus-iOS/GradesComponent/Screen/GradesScreen.swift
+++ Campus-iOS/GradesComponent/Screen/GradesScreen.swift
@@ -26,70 +26,73 @@ struct GradesScreen: View {
     }
     
     var body: some View {
-        Group {
-            switch vm.state {
-            case .success(_):
-                if vm.grades.count <= 0 {
-                    FailedView(
-                            errorDescription: "No grades yet!",
-                            retryClosure: vm.reloadGradesAndAverageGrades
-                        )
-                } else {
+        if (vm.grades.count > 0) {
+            Group {
+                switch vm.state {
+                case .success(_):
                     VStack {
-                        GradesView(
-                            vm: self.vm
-                        )
-                        .padding(.top, 30)
-                        .refreshable {
-                            await vm.reloadGradesAndAverageGrades(forcedRefresh: true)
+                        if (vm.grades.count <= 0) {
+                            EmptyGradesView()
+                        } else {
+                            GradesView(
+                                vm: self.vm
+                            )
+                            .padding(.top, 30)
+                            .refreshable {
+                                await vm.reloadGradesAndAverageGrades(forcedRefresh: true)
+                            }
+                        }
+                        
+                    }
+                case .loading, .na:
+                    LoadingView(text: "Fetching Grades")
+                case .failed(let error):
+                    FailedView(
+                        errorDescription: error.localizedDescription,
+                        retryClosure: vm.reloadGradesAndAverageGrades
+                    )
+                }
+            }
+            .task {
+                await vm.reloadGradesAndAverageGrades()
+            }
+            // Refresh whenever user authentication status changes
+            .onChange(of: self.refresh) { _ in
+                Task {
+                    await vm.reloadGradesAndAverageGrades()
+                }
+            }
+            // As LoginView is just a sheet displayed in front of the GradeScreen
+            // Listen to changes on the token, then fetch the grades
+            .onChange(of: self.vm.model.token ?? "") { _ in
+                Task {
+                    await vm.reloadGradesAndAverageGrades()
+                }
+            }
+            .alert(
+                "Error while fetching Grades",
+                isPresented: $vm.hasError,
+                presenting: vm.state) { detail in
+                    Button("Retry") {
+                        Task {
+                            await vm.reloadGradesAndAverageGrades()
+                        }
+                    }
+                    
+                    Button("Cancel", role: .cancel) { }
+                } message: { detail in
+                    if case let .failed(error) = detail {
+                        if let apiError = error as? TUMOnlineAPIError {
+                            Text(apiError.errorDescription ?? "TUMOnlineAPI Error")
+                        } else {
+                            Text(error.localizedDescription)
                         }
                     }
                 }
-            case .loading, .na:
-                LoadingView(text: "Fetching Grades")
-            case .failed(let error):
-                FailedView(
-                    errorDescription: error.localizedDescription,
-                    retryClosure: vm.reloadGradesAndAverageGrades
-                )
-            }
+        } else {
+            EmptyGradesView()
         }
-        .task {
-            await vm.reloadGradesAndAverageGrades()
-        }
-        // Refresh whenever user authentication status changes
-        .onChange(of: self.refresh) { _ in
-            Task {
-                await vm.reloadGradesAndAverageGrades()
-            }
-        }
-        // As LoginView is just a sheet displayed in front of the GradeScreen
-        // Listen to changes on the token, then fetch the grades
-        .onChange(of: self.vm.model.token ?? "") { _ in
-            Task {
-                await vm.reloadGradesAndAverageGrades()
-            }
-        }
-        .alert(
-            "Error while fetching Grades",
-            isPresented: $vm.hasError,
-            presenting: vm.state) { detail in
-                Button("Retry") {
-                    Task {
-                        await vm.reloadGradesAndAverageGrades()
-                    }
-                }
-                
-                Button("Cancel", role: .cancel) { }
-            } message: { detail in
-                if case let .failed(error) = detail {
-                    if let apiError = error as? TUMOnlineAPIError {
-                        Text(apiError.errorDescription ?? "TUMOnlineAPI Error")
-                    } else {
-                        Text(error.localizedDescription)
-                    }
-                }
-            }
+        
     }
 }
 
